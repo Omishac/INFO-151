e5317eb4ae5374052925bf22692cee702f3d0ac7
Omishac <oc86@drexel.edu>
2023-12-04 14:33:02 -0500

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,33 @@ var SeeMainNav = false;
 function showNav() {
     console.log("button clicked");
 
-    if (!SeeMainNav) {
-        console.log("show menu");
-        mainNav.classList.add("showMainNav");
-        signIn.classList.add("showSignIn");
-        SeeMainNav = true;
-    } else {
-        console.log("hide menu");
-        mainNav.classList.remove("showMainNav");
-        signIn.classList.remove("showSignIn");
-        SeeMainNav = false;
+    // Check if mainNav and signIn elements exist
+    if (mainNav && signIn) {
+        if (!SeeMainNav) {
+            console.log("show menu");
+            mainNav.classList.add("showMainNav");
+            signIn.classList.add("showSignIn");
+            SeeMainNav = true;
+        } else {
+            console.log("hide menu");
+            mainNav.classList.remove("showMainNav");
+            signIn.classList.remove("showSignIn");
+            SeeMainNav = false;
+        }
     }
 }
 
 burgerBtn.addEventListener("click", showNav);
+
+var signInBtn = document.querySelector("#sign-in-btn");
+
+function showSignInModal() {
+    console.log("Show sign-in modal now");
+
+
+}
+
+
+if (signInBtn) {
+    signInBtn.addEventListener("click", showSignInModal);
+}
